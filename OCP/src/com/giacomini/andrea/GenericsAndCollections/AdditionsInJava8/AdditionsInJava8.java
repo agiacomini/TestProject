9f5ffa617d89a6c68b1e4c80540e73fe236a2969
A.G <andr3a.giacomini@gmail.com>
2018-12-22 15:04:31 +0100

    update AdditionalJava8

--- OCP/src/com/giacomini/andrea/GenericsAndCollections/AdditionsInJava8/AdditionsInJava8.java
+++ OCP/src/com/giacomini/andrea/GenericsAndCollections/AdditionsInJava8/AdditionsInJava8.java
@@ -1,5 +1,13 @@
 package com.giacomini.andrea.GenericsAndCollections.AdditionsInJava8;
 
+/*
+ * N.B: In questo capitolo verrà introdotto il concetto di "method references" (riferimento a metodo)
+ * 		per mostrare come rendere il codice più compatto. "Method references" e le lambda expression
+ * 		sono il cuore di Java 8 e per questo motivo ci si aspetta di ritrovarle anche nei prossimi
+ * 		capitoli. Mostreremo come usare i metodi "removeIf()", "merge()", "computeIfPresent()" e 
+ * 		"computeIfAbsent()".
+ * */
+
 public class AdditionsInJava8 {
 
 }
