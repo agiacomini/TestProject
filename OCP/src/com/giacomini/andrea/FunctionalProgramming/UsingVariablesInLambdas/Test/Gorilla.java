2b69e45066dd6e0f1c04339aed4db380e4c54a89
A.G <andr3a.giacomini@gmail.com>
2019-01-15 07:30:50 +0100

    commit from intellij. Add interface test example "Gorilla.java"

---
+++ OCP/src/com/giacomini/andrea/FunctionalProgramming/UsingVariablesInLambdas/Test/Gorilla.java
@@ -0,0 +1,9 @@
+package com.giacomini.andrea.FunctionalProgramming.UsingVariablesInLambdas.Test;
+
+/**
+ * Created by Andrea on 12/01/2019.
+ */
+public interface Gorilla {
+
+    String move();
+}
