919b52854ccb5bf230897235bae5b49d7f052aab
A.G <andr3a.giacomini@gmail.com>
2020-01-18 20:42:19 +0100

    Update sign with @e-mail, @pkg and @prj info

--- EffectiveJavaSecondEdition_JoshuaBloch/src/com/giacomini/andrea/utils/ProjectLogger.java
+++ EffectiveJavaSecondEdition_JoshuaBloch/src/com/giacomini/andrea/utils/ProjectLogger.java
@@ -2,6 +2,13 @@ package com.giacomini.andrea.utils;
 
 import org.apache.log4j.Logger;
 
+/**
+ * 
+ * @author com.giacomini.andrea.utilsAndrea
+ * @e-mail andr3a.giacomini@gmail.com
+ * @pkg	   com.giacomini.andrea.utils, @prj EffectiveJavaSecondEdition_JoshuaBloch
+ *
+ */
 public class ProjectLogger 
 {
 	public static transient Logger logger = Logger.getLogger( Logger.class );
